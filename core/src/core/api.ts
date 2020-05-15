41dc9f67a5f458eb90d256e47d700dcd46459fe0
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2020-05-15 12:27:53 +0800

    在 load、routes 新增 middleware 機制。

--- core/src/core/api.ts
+++ core/src/core/api.ts
@@ -1,6 +1,6 @@
 import Koa from 'koa';
 import Router from 'koa-router';
-
+import { Middleware} from "../reflection/consts";
 import { loadPackageFromFolder, normalizeRouteName } from './utils';
 import { ServiceConfigImpl } from '../reflection/service_config_impl';
 import { StaticProvider, Injector } from '../di';
@@ -17,9 +17,12 @@ export class API {
      * 以 Koa 格式輸出 Contract 所有路由路徑。
      * @param path 基礎路徑，不指定即為沒有基礎路徑。
      */
-    public routes(path?: string): Koa.Middleware {
+    public routes(path?: string, middleware?: Middleware[]): Koa.Middleware {
         if (path) {
             const base = new Router();
+
+            if (middleware) { base.use(...middleware); }
+
             base.use(path, this.router.routes());
             return base.routes();
         } else {
@@ -31,10 +34,14 @@ export class API {
      * 從指定路徑載入 Contract 所有 Package、Service。
      * @param dirPath 路徑。
      */
-    public static async load(dirPath: string, options?: { providers: StaticProvider[] }): Promise<API> {
+    public static async load(dirPath: string, options?: { providers: StaticProvider[], middleware?: Middleware[] }): Promise<API> {
 
         const api = new API();
         const apiRouter = api.router;
+
+        if(options?.middleware) {
+            apiRouter.use(...options.middleware);
+        }
 
         const rootInjector = Injector.create(options || { providers: [] });
 
